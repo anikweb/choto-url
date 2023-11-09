dfb2191ed71925dee7e5db4eb04d001c1bb84413
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-11-10 01:35:36 +0600

    form design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,13 @@
 <html lang="en">
 
 <head>
+    <title>Choto URL</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Choto URL</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <style>
         body {
             background-color: #f5f5f5;
@@ -24,13 +25,25 @@
 
     <div class="container-fluid">
         <div class="row d-flex justify-content-center align-items-center" style="height: 100vh">
-            <div class="col-md-6">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-header text-center">
-                        Choto URL
+                        <h3 class="text-success">Choto URL</h3>
                     </div>
                     <div class="card-body">
+                        <div class="form group my-2 ">
+                            <label for="long_url"><i class="fa-solid fa-link"></i> Long URL</label>
+                            <input type="text" id="long_url" class="form-control"
+                                placeholder="Enter your long URL here">
+                        </div>
+                        <div class="form group my-2">
+                            <label for="long_url"> Alias <span>Optional</span></label>
+                            <input type="text" id="long_url" class="form-control" placeholder="Enter alias">
+                        </div>
 
+                        <div class="form-group text-center my-2">
+                            <button class="btn btn-success text-center">Shorten URL</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,6 +54,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js""></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
     <script>
         $(document).ready(function() {
             $('p').click(function() {
