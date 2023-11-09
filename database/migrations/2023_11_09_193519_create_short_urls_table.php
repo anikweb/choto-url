807296f035353a82b0f0a595eb36a51c025133de
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-11-10 01:37:46 +0600

    design migration schema for short url table

---
+++ database/migrations/2023_11_09_193519_create_short_urls_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create( 'short_urls', function ( Blueprint $table ) {
+            $table->id();
+            $table->longText( 'long_url' )->unique();
+            $table->longText( 'short_url' )->unique();
+            $table->timestamps();
+        } );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists( 'short_urls' );
+    }
+};
