011de4a7c64506f79bc939daa582c9bbe8b56d1f
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-11-10 01:45:21 +0600

    update model

---
+++ app/Models/ShortUrl.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ShortUrl extends Model {
+    use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'long_url',
+        'short_url',
+    ];
+}
