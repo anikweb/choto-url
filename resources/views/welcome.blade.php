44a723d2a5047d5501000acdc85010d15c383f38
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-11-11 02:04:47 +0600

    form re-design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,9 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
@@ -26,19 +28,6 @@
             background-position: center;
             background-attachment: fixed;
         }
-
-        .btn-check:focus+.btn,
-        .btn:focus {
-            border-color: transparent;
-            box-shadow: 0 0 0 0.25rem transparent;
-        }
-
-        /* set all font family to Agbalumo */
-
-        .card-header,
-        .modal-header {
-            font-family: 'Agbalumo', cursive;
-        }
     </style>
 </head>
 
@@ -49,17 +38,17 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header text-center">
-                        <h3 class="text-success">Choto URL</h3>
+                        <h2 class="text-success">Choto URL</h2>
                     </div>
                     <div class="card-body">
                         <div id="error-message"></div>
                         <div class="form group my-2 long_url_wrapper">
-                            <label for="long_url"><i class="fa-solid fa-link"></i> Long URL</label>
+                            <label for="long_url"><i class="lni lni-link"></i> Long URL</label>
                             <input type="url" id="long_url" class="form-control"
                                 placeholder="Enter your long URL here">
                         </div>
                         <div class="form group my-2 alias_wrapper">
-                            <label for="alias"> Alias <span>Optional</span></label>
+                            <label for="alias"> Alias <span><small>(Optional)</small></span></label>
                             <input type="text" id="alias" class="form-control" placeholder="Enter alias">
                         </div>
                         <div class="form group my-2 d-none choto_url_wrapper">
@@ -71,7 +60,7 @@
                         <div class="generatedBtn"></div>
 
                         <div class="form-group text-center my-2 submit_btn_wrapper">
-                            <button id="submit_url" class="btn btn-success text-center">Shorten URL</button>
+                            <button id="submit_url" class="btn main-btn btn-success text-center">Shorten URL</button>
                         </div>
                     </div>
                 </div>
@@ -87,7 +76,7 @@
             <div class="modal-content">
                 <div class="modal-header bg-success text-white">
                     <h5 class="modal-title" id="qrModalLabel">QR Code</h5>
-                    <button type="button" class="close btn text-white closeModalButton" data-dismiss="modal"
+                    <button type="button" class="close main-btn btn text-white closeModalButton" data-dismiss="modal"
                         aria-label="Close">
                         <span aria-hidden="true"><i class="fa-solid fa-xmark"></i></span>
                     </button>
@@ -97,8 +86,8 @@
                 </div>
                 <div class="modal-footer d-flex justify-content-center">
 
-                    <a id="downloadLink" class="btn btn-success" download="qrcode.png"><i
-                            class="fa-solid fa-download"></i> Download</a>
+                    <a id="downloadLink" class="btn btn-success" download="qrcode.png"><i class="lni lni-download"></i>
+                        Download</a>
                 </div>
             </div>
         </div>
@@ -139,7 +128,7 @@
                                 $("#long_url").attr('disabled', 'disabled');
 
                                 $(".submit_btn_wrapper").html(
-                                    `<a  href="{{ url('/') }}" class="btn btn-success text-center">Shorten Another</a>`
+                                    `<a  href="{{ url('/') }}" class="btn btn-success text-center main-btn">Shorten Another</a>`
                                 )
 
                                 // generated btn
@@ -150,7 +139,7 @@
                             } else {
                                 let errorHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
                             <span>${response.message}</span>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
+                            <button type="button" class="btn-close main-btn" data-bs-dismiss="alert"
                                 aria-label="Close"></button>
                         </div>`
                                 $("#error-message").html(errorHTML)
@@ -165,7 +154,7 @@
                             console.log(error);
                             let errorHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
                             <span>${error.responseJSON.message}</span>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
+                            <button type="button" class="btn-close main-btn" data-bs-dismiss="alert"
                                 aria-label="Close"></button>
                         </div>`
                             $("#error-message").html(errorHTML)
@@ -193,10 +182,11 @@
         }
 
         function generateBtn(url) {
-            let html = `
-        <button onclick="copyToClipboard('${url}', this)" class="btn btn-success" data-toggle="tooltip" data-placement="top" title="Copied!"><i class="fa-solid fa-copy"></i> Copy</button>
-        <button onclick="generateQRCode('${url}',this)" class="btn btn-primary"><i class="fa-solid fa-qrcode"></i> QR Code</button>
-        <a href="${url}" target="_blank" class="btn btn-primary"><i class="fa-solid fa-share"></i> Visit</a>`;
+            let html =
+                `
+        <button onclick="copyToClipboard('${url}', this)" class="btn secondary-btn btn-success" data-toggle="tooltip" data-placement="top" title="Copied!"><i class="lni lni-clipboard"></i> Copy</button>
+        <button onclick="generateQRCode('${url}',this)" class="btn secondary-btn btn-primary"><i class="fa-solid fa-qrcode"></i> QR Code</button>
+        <a href="${url}" target="_blank" class="btn btn-primary secondary-btn"><i class="fa-solid fa-share"></i> Visit</a>`;
             return html;
         }
 
