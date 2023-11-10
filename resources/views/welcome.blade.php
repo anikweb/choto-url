2222fde4ac3d6e21d00a4f4edc18980aadb34475
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-11-11 01:42:42 +0600

    add new font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="{{ asset('assets/css/loader.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <style>
@@ -27,6 +31,13 @@
         .btn:focus {
             border-color: transparent;
             box-shadow: 0 0 0 0.25rem transparent;
+        }
+
+        /* set all font family to Agbalumo */
+
+        .card-header,
+        .modal-header {
+            font-family: 'Agbalumo', cursive;
         }
     </style>
 </head>
