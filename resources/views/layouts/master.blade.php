a904aa489a7de939cf73429a4b27b26a9b2b6f6f
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-11-11 04:21:47 +0600

    add copyright footer text

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,15 +1,83 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <title>Choto URL</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {{-- SEO --}}
+    <meta name="description" content="Make your long URL to short URL easily.">
+    <meta name="keywords"
+        content="choto url, url shortener, short url, url shortener free, url shortener google, url shortener custom, url shortener custom domain, url shortener custom link, url shortener custom name, url shortener custom url, url shortener c" />
+    <meta name="author" content="Anik Kumar Nandi ">
+
+    {{-- Add canonical meta --}}
+
+    <link rel="canonical" href="{{ url('/') }}">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="Choto URL">
+    <meta property="og:url" content="{{ url('/') }}">
+    <meta property="og:title" content="Choto URL">
+    <meta property="og:description" content="Make your long URL to short URL easily.">
+    <meta property="og:image" content="{{ asset('assets/img/featured_image.jpeg') }}">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="Choto URL">
+    <meta property="twitter:url" content="{{ url('/') }}">
+    <meta property="twitter:title" content="Choto URL">
+    <meta property="twitter:description" content="Make your long URL to short URL easily.">
+    <meta property="twitter:image" content="{{ asset('assets/img/featured_image.jpeg') }}">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+
+    {{-- Add favicon --}}
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
+    {{-- Add font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/loader.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <style>
+        body {
+            background-color: #f5f5f5;
+            background: url("{{ asset('assets/img/background.webp') }}");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            background-attachment: fixed;
+        }
+    </style>
 </head>
 
 <body>
+    {{-- Main Content --}}
+    @yield('content')
+
+    <footer>
+        <div class="row copyright-footer">
+            <div class="col-md-">
+                <p class="text-white">&copy; All Right Reserved by <a class="text-white" target="_blank"
+                        href="https://www.linkedin.com/in/anikprogrammer/">Anik Kumar Nandi</a></p>
+            </div>
+        </div>
+
+    </footer>
+
+
+    {{-- Scripts --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js""></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
+
+    @stack('scripts')
 
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,69 +1,5 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Choto URL</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{-- SEO --}}
-    <meta name="description" content="Make your long URL to short URL easily.">
-    <meta name="keywords" content="choto url, url shortener, short url, url shortener free, url shortener google, url shortener custom, url shortener custom domain, url shortener custom link, url shortener custom name, url shortener custom url, url shortener c"/>
-    <meta name="author" content="Anik Kumar Nandi ">
-
-    {{-- Add canonical meta --}}
-
-    <link rel="canonical" href="{{ url('/') }}">
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="Choto URL">
-    <meta property="og:url" content="{{ url('/') }}">
-    <meta property="og:title" content="Choto URL">
-    <meta property="og:description" content="Make your long URL to short URL easily.">
-    <meta property="og:image" content="{{ asset('assets/img/featured_image.jpeg') }}">
-    
-
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="Choto URL">
-    <meta property="twitter:url" content="{{ url('/') }}">
-    <meta property="twitter:title" content="Choto URL">
-    <meta property="twitter:description" content="Make your long URL to short URL easily.">
-    <meta property="twitter:image" content="{{ asset('assets/img/featured_image.jpeg') }}">
-
-
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-
-    {{-- Add favicon --}}
-    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
-
-    {{-- Add font --}}
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Agbalumo&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="{{ asset('assets/css/loader.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-    <style>
-        body {
-            background-color: #f5f5f5;
-            background: url("{{ asset('assets/img/background.webp') }}");
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center;
-            background-attachment: fixed;
-        }
-    </style>
-</head>
-
-<body>
-
+@extends('layouts.master')
+@section('content')
     <div class="container-fluid">
         <div class="row d-flex justify-content-center align-items-center" style="height: 100vh">
             <div class="col-xl-5 col-md-8 col-12">
@@ -100,14 +36,13 @@
 
     {{-- QR Modal  --}}
 
-    <div class="modal fade " id="qrModal" tabindex="-1" role="dialog" aria-labelledby="qrModalLabel"
-        aria-hidden="true">
+    <div class="modal fade " id="qrModal" tabindex="-1" role="dialog" aria-labelledby="qrModalLabel" aria-hidden="true">
         <div class="modal-dialog  modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header bg-success text-white">
                     <h5 class="modal-title" id="qrModalLabel">QR Code</h5>
-                    <button type="button" class="close main-btn btn text-white closeModalButton"
-                        data-dismiss="modal" aria-label="Close">
+                    <button type="button" class="close main-btn btn text-white closeModalButton" data-dismiss="modal"
+                        aria-label="Close">
                         <span aria-hidden="true"><i class="fa-solid fa-xmark"></i></span>
                     </button>
                 </div>
@@ -116,18 +51,15 @@
                 </div>
                 <div class="modal-footer d-flex justify-content-center">
 
-                    <a id="downloadLink" class="btn btn-success" download="qrcode.png"><i
-                            class="lni lni-download"></i>
+                    <a id="downloadLink" class="btn btn-success" download="qrcode.png"><i class="lni lni-download"></i>
                         Download</a>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js""></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
+@endsection
 
-
+@push('scripts')
     <script>
         $(document).ready(function() {
             $('#submit_url').click(function(event) {
@@ -282,6 +214,4 @@
             $('#qrModal').modal('hide');
         });
     </script>
-</body>
-
-</html>
+@endpush
